325efc90cb999ca746f04ea8ab753de13d2620dc
Taylor Monacelli <taylormonacelli@gmail.com>
2017-11-28 11:30:12 -0800

    ws

--- recipes/terraform.rb
+++ recipes/terraform.rb
@@ -5,12 +5,12 @@ template "#{ENV['PWD']}/env_#{node.chef_environment}.tvars" do
   source 'terraform.tvars.erb'
   sensitive true
   variables(
-    aws_access_key_id:         wordpress_creds[node.chef_environment]['dns_aws_access_key_id'],
-    aws_secret_access_key:     wordpress_creds[node.chef_environment]['dns_aws_secret_access_key'],
+    aws_access_key_id:             wordpress_creds[node.chef_environment]['dns_aws_access_key_id'],
+    aws_secret_access_key:         wordpress_creds[node.chef_environment]['dns_aws_secret_access_key'],
     aws_cloudfront_cname:      "#{node['terraform_hostname'] || node['hostname']}cdn.#{node['terraform_domain'] || node['domain']}".downcase,
-    mysql_root_username:       database_creds[node.chef_environment]['mysql_root_username'],
-    mysql_root_password:       database_creds[node.chef_environment]['mysql_root_password'],
-    mysql_wordpress_username:  database_creds[node.chef_environment]['db_user'],
+    mysql_root_username:           database_creds[node.chef_environment]['mysql_root_username'],
+    mysql_root_password:           database_creds[node.chef_environment]['mysql_root_password'],
+    mysql_wordpress_username:      database_creds[node.chef_environment]['db_user'],
     mysql_wordpress_password:  database_creds[node.chef_environment]['db_password']
   )
 end
