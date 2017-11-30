45cbc917903cbc96505fbdadf5f9b4db13e5fbba
Taylor Monacelli <taylormonacelli@gmail.com>
2017-11-30 10:00:54 -0800

    flail

--- json_config.tf
+++ json_config.tf
@@ -1,12 +1,10 @@
 data "template_file" "init" {
   template = <<EOF
   {
-    "db_endpoint": "$${db_endpoint}"
-   ,"set_fqdn": "${var.fqdn}"
+   "set_fqdn": "${var.fqdn}"
   }
   EOF
 
   vars {
-    db_endpoint       = "${aws_db_instance.default.endpoint}"
   }
 }
