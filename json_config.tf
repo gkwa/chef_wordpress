8c62a8da113815db09bbd54ec25bf64a0ec51a45
Taylor Monacelli <taylormonacelli@gmail.com>
2017-11-29 21:55:14 -0800

    Revert me: need to iterate faster by removing cloudfront
    Destroying cloudfront takes 10 minutes. I need to skip that for now.
    Once I get solid RDS running I will revert this.

--- json_config.tf
+++ json_config.tf
@@ -2,13 +2,11 @@ data "template_file" "init" {
   template = <<EOF
   {
     "db_endpoint": "$${db_endpoint}"
-   ,"cloudfront_domain": "$${cloudfront_domain}"
    ,"set_fqdn": "${var.fqdn}"
   }
   EOF
 
   vars {
     db_endpoint       = "${aws_db_instance.default.endpoint}"
-    cloudfront_domain = "${aws_s3_bucket.b1.bucket_domain_name}"
   }
 }
